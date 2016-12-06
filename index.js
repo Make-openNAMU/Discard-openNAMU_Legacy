ff073c317dbda579f34ec6f759b4f4a55f977ce3
zlzleking <zlzleking@gmail.com>
2016-12-07 08:40:07 +0900

    퍼미션 학살

--- index.js
+++ index.js
@@ -970,11 +970,11 @@ router.post('/topic/:page/:topic', function(req, res) {
 		    var yfile = './topic/' + encodeURIComponent(req.params.page) + '/';
 		    var exists = fs.existsSync(yfile);
 		    if(!exists) {
-			  fs.mkdirSync('./topic/' + encodeURIComponent(req.params.page), 777);
+			  fs.mkdirSync('./topic/' + encodeURIComponent(req.params.page), 0755);
 		    }
 		    var exists = fs.existsSync(rfile);
 		    if(!exists) {
-		  	  fs.mkdirSync('./topic/' + encodeURIComponent(req.params.page) + '/' + encodeURIComponent(req.params.topic), 777);
+		  	  fs.mkdirSync('./topic/' + encodeURIComponent(req.params.page) + '/' + encodeURIComponent(req.params.topic), 0755);
 		    }
 		    else {
 			  var number = fs.readFileSync(nfile, 'utf8');
@@ -2504,7 +2504,7 @@ router.post('/edit/:page', function(req, res) {
 					fs.writeFileSync('./data/' + encodeURIComponent(req.params.page)+'.txt', req.body.content, 'utf8');
 					var exists = fs.existsSync('./history/' + encodeURIComponent(req.params.page) + '/r1.txt');
 					if(!exists) {
-						fs.mkdirSync('./history/' + encodeURIComponent(req.params.page), 777);
+						fs.mkdirSync('./history/' + encodeURIComponent(req.params.page), 0755);
 						fs.openSync('./history/' + encodeURIComponent(req.params.page) + '/r1.txt','w+');
 						fs.writeFileSync('./history/' + encodeURIComponent(req.params.page) + '/r1.txt', req.body.content, 'utf8');
 						fs.openSync('./history/' + encodeURIComponent(req.params.page) + '/r1-ip.txt','w+');
